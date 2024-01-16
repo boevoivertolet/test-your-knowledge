01034497a6a53dbd539613e66451e1d0bca457f1
boevoivertolet <bjha78rus@yandex.ru>
2024-01-16 16:27:06 +0300

    first commit

---
+++ src/data/data.ts
@@ -0,0 +1,3 @@
+export const data: DataType = [{ id: 1, question: 'question', answer: 'answer' }]
+type QuestionType = { id: number; question: string; answer: string }
+type DataType = QuestionType[]
